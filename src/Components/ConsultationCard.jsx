ca03b87bcf35f1c90b20298699abee8edd41be64
David-Saini <lakshaysaini92558@gmail.com>
2024-01-16 14:14:01 +0530

    font updates

--- src/Components/ConsultationCard.jsx
+++ src/Components/ConsultationCard.jsx
@@ -11,16 +11,16 @@ const ConsultationCard = () => {
             <div className='relative'>
                 <Image className='absolute top-[-80%] start-[-20%] lg:top-[-100%] lg:start-[-30%]' src={ellipse_67}></Image>
                 <Image className='absolute bottom-[-80%] xl:bottom-[-64%] end-[0%] xl:end-[-40%]' src={ellipse_64}></Image>
-                <Image className='absolute lg:bottom-[-100%] xl:bottom-[-60%] start-[0%]' src={ellipse_68}></Image>
+                <Image className='absolute bottom-0 lg:bottom-[-100%] xl:bottom-[-60%] start-[0%]' src={ellipse_68}></Image>
                 <div className='pb-12 md:pb-[60px] lg:pb-20 xl:pb-[150px] mx-auto max-w-[1140px] px-3 relative z-[50]'>
-                    <div className='max-w-[650px] lg:max-w-[1140px] mx-auto !w-full border-white rounded-[22px] bg-white overflow-hidden'>
+                    <div className='max-w-[650px] lg:max-w-[1140px] mx-auto !w-full border-white rounded-[22px] bg-white overflow-hidden consltation_card_shadow mt-12 sm:mt-0'>
                         <div className='flex items-center justify-between flex-col-reverse lg:flex-row gap-6 sm:gap-8 md:gap-12 lg:gap-[80px] xl:gap-[150px]'>
 
                             <div className='px-3 sm:pe-0 lg:ps-[48px] xl:ps-[72px]'>
-                                <h3 className='font-Matter text-[40px] font-semibold leading-[135%] tracking-[-0.8%] text-secondblack'>Get Free Consultation</h3>
+                                <h3 className='font-Matter text-[32px] md:text-[38px] lg:text-[40px] font-semibold leading-[135%] tracking-[-0.8%] text-secondblack'>Get Free Consultation</h3>
                                 <p className='font-Matter text-[18px] font-normal leading-[155%] text-secondblack mt-2 lg:mt-4'>Get in touch today to qualify for a FREE personalized marketing plan.</p>
                                 <div className='mb-3 mt-6 sm:mb-6 md:mb-8 md:mt-8 lg:mb-0'>
-                                    <button className="bg-purple leading-[120%] hover:bg-white hover:text-purple hover:border-[2px] border-solid border-purple duration-150 border-[2px] font-Matter font-semibold text-[18px] rounded-[8px] w-[189px] whitespace-nowrap h-[62px] text-white py-[19px]  px-[31px]">Download Now</button>
+                                    <button className="bg-purple leading-[120%] hover:bg-white hover:text-purple hover:border-[2px] border-solid border-purple duration-150 border-[2px] font-Matter font-semibold text-[16px] md:text-[18px] rounded-[8px] w-[150px] h-[52px] md:w-[189px] whitespace-nowrap md:h-[62px] text-white py-[15px] md:py-[19px]  px-[15px] md:px-[31px]">Download Now</button>
                                 </div>
                             </div>
                             <Image width={461} height={328} src={man_img} alt='man_img'></Image>
--- src/Components/StrategiesCard.jsx
+++ src/Components/StrategiesCard.jsx
@@ -9,12 +9,12 @@ import ellipse_61 from "../../public/images/webp/Ellipse_61.webp"
 const StrategiesCard = () => {
     return (
         <div className='max-w-[1920px] mx-auto'>
-            <div className='relative z-[-1]'>
+            <div className='relative z-[1]'>
                 <Image className='absolute md:top-[-130%] lg:top-[-40%] md:end-[-20%] lg:end-[-30%]' src={ellipse_65} alt=''></Image>
                 <Image className='absolute top-[-200%] lg:top-[10%] md:end-[-10%] lg:end-[0%]' src={ellipse_64} alt='ellipse_64'></Image>
                 <Image className='absolute top-[-60%] lg:top-[-40%] start-[-20%]' src={ellipse_62} alt='ellipse_62'></Image>
                 <Image className='absolute top-[10%] start-[-10%]' src={ellipse_61} alt='ellipse_61'></Image>
-                <div className='mb-12 md:mb-[60px] lg:mb-20 xl:mb-[124px] relative z-10'>
+                <div className='mb-12 md:mb-[60px] lg:mb-20 xl:mb-[124px] relative z-10  mt-12 sm:mt-0'>
                     <div className='max-w-[1140px] mx-auto px-3'>
                         <div className='max-w-[1140px] w-full rounded-[30px] skyblue_bg p-6 md:p-[34px]'>
                             <div className='flex items-center justify-center flex-wrap'>
@@ -25,9 +25,9 @@ const StrategiesCard = () => {
 
                                 <div className='lg:w-7/12 mt-8 sm:mt-10 md:mt-12 lg:mt-0 lg:ps-12'>
                                     <p className='text-white ff_montserat text-[24px] font-semibold leading-[135%] tracking-[-0.48px] uppercase'>Free Guide</p>
-                                    <h3 className='text-white font-Matter text-[40px] font-semibold leading-[135%] tracking-[-0.8px] mt-4'>Avoid Deadly Online Marketing Mistakes With These 7 Key Strategies</h3>
-                                    <div className='max-w-[189px] h-[62px] mt-4 sm:mt-8 md:mt-12'>
-                                        <button className='font-Matter font-semibold leading-[120%] text-[18px] text-secondblack rounded-lg bg-Mustard_color w-full py-5'>Download Now</button>
+                                    <h3 className='text-white font-Matter text-[27px] sm:text-[32px] md:text-[35px] lg:text-[40px] font-semibold leading-[135%] tracking-[-0.8px] mt-4'>Avoid Deadly Online Marketing Mistakes With These 7 Key Strategies</h3>
+                                    <div className='md:max-w-[189px] md:h-[62px] max-w-[150px] h-[52px] mt-4 sm:mt-8 md:mt-12'>
+                                        <button className='font-Matter font-semibold leading-[120%] md:text-[18px] text-secondblack rounded-lg bg-Mustard_color w-full py-4 md:py-5'>Download Now</button>
                                     </div>
                                 </div>
 
