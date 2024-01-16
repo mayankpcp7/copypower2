b8b9c9e3aeb4b7666c1d5a1b6ca9f84ca3268902
David-Saini <lakshaysaini92558@gmail.com>
2024-01-16 15:32:33 +0530

    code merge

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,12 @@ import ConsultationCard from "@/src/Components/ConsultationCard";
 import StrategiesCard from "@/src/Components/StrategiesCard";
 import Footer from "@/src/Components/Footer";
 import BackToTop from "@/src/Components/BackToTop";
-import Pre_loder from "@/src/Components/Pre_loader";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <div className="overflow-hidden">
-        <Pre_loder />
         <NavBar />
         <Header />
         <WeWork />
