fa46a43fd7b8e474813d8983959caada45cc7d11
mayankpcp7 <jariwalmayank123@gmail.com>
2024-01-16 12:25:27 +0530

    merge code

--- src/Components/WeWork.jsx
+++ src/Components/WeWork.jsx
@@ -35,29 +35,30 @@ const WeWork = () => {
   return (
     <>
       <div>
-        <section className="pt-[44px] bg-work">
+        <section className="pt-[44px]">
           <div ref={triggerRef}>
             <div ref={sectionRef} className="container ">
-              <h2 className="text-main font-Matter font-semibold xl:text-[40px] leading-[135%] text-center max-w-[797px] mx-auto">
+              <h2 className="text-main font-Matter font-semibold text-[28px] md:text-[33px] lg:text-[35px] xl:text-[40px] leading-[135%] text-center max-w-[797px] mx-auto">
                 We work with you to create scalable, repeatable full-stack
                 marketing
               </h2>
-              <div className="flex content-wrapper justify-center flex-wrap items-center md:justify-between mt-[66px] h-screen overflow-x-hidden">
+              <div className="flex content-wrapper sticky justify-center flex-wrap items-center md:justify-between mt-[66px] h-screen overflow-x-hidden">
                 {work.map((process) => {
                   return (
                     <>
                       <div
-                        className="md:w-6/12 w-10/12 px-3 mb-[50px]  md:mb-[100px] xl:mb-[140px]"
+                        className="md:w-6/12 w-10/12 px-3 mb-[50px] sticky  md:mb-[100px] xl:mb-[140px]"
                         key={process.id}
                       >
                         <Image
+                          classsName="sticky"
                           src={process.img}
                           width={512}
                           height={447}
                         ></Image>
                       </div>
                       <div
-                        className="md:w-5/12 w-10/12 px-3 md:mb-[100px] mb-[70px] xl:mb-[140px]"
+                        className="md:w-5/12 w-10/12 sticky px-3 md:mb-[100px] mb-[70px] xl:mb-[140px]"
                         key={process.id}
                       >
                         <h4 className="text-start text-black font-Matter text-[28px] leading-[140%]">
@@ -70,7 +71,7 @@ const WeWork = () => {
                             width={28}
                             height={28}
                           ></Image>
-                          <p className="font-matter text-secondblack text-[20px] leading-[145%] font-normal">
+                          <p className="font-matter text-secondblack text-[18px] lg:text-[20px] leading-[145%] font-normal">
                             {process.pera1}
                           </p>
                         </div>
@@ -81,7 +82,7 @@ const WeWork = () => {
                             width={28}
                             height={28}
                           ></Image>
-                          <p className="font-matter text-secondblack text-[20px] leading-[145%] font-normal">
+                          <p className="font-matter text-secondblack text-[18px] lg:text-[20px] leading-[145%] font-normal">
                             {process.pera2}
                           </p>
                         </div>
@@ -92,13 +93,13 @@ const WeWork = () => {
                             width={28}
                             height={28}
                           ></Image>
-                          <p className="font-matter text-secondblack text-[20px] leading-[145%] font-normal">
+                          <p className="font-matter text-secondblack text-[18px] lg:text-[20px] leading-[145%] font-normal">
                             {process.pera3}
                           </p>
                         </div>
                         <div className="flex items-center mt-[13px]">
                           <div>{process.check2}</div>
-                          <p className="font-matter text-secondblack text-[20px] leading-[145%] font-normal">
+                          <p className="font-matter text-secondblack text-[18px] lg:text-[20px] leading-[145%] font-normal">
                             {process.pera4}
                           </p>
                         </div>
